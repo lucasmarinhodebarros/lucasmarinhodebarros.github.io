81a4e14699d47dcc80d9ed8c50ac72f08ba90552
lucasdebarros <136212152+lucasdebarros@users.noreply.github.com>
2023-07-07 00:33:49 -0300

    scriptstyle.js

--- docs/scriptstyle.js
+++ docs/scriptstyle.js
@@ -1,22 +1,3 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    if (menu.style.display === "none" || menu.style.display === "") {
-      menu.style.display = "block";
-    } else {
-      menu.style.display = "none";
-    }
-  }
-  
-  window.onclick = function(event) {
-    var menu = document.getElementById("menu");
-    var button = document.querySelector(".dropdown button");
-    if (!event.target.matches(".dropdown button")) {
-      if (menu.style.display === "block") {
-        menu.style.display = "none";
-      }
-    }
-  }
-
 window.addEventListener('load', function () {
             var loadingScreen = document.querySelector('.loading-screen');
             loadingScreen.style.display = 'none';
