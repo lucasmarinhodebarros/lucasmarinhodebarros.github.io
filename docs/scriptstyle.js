b77d2dd2237243d1af403e70b1068c6af68eeb4c
lucasdebarros <136212152+lucasdebarros@users.noreply.github.com>
2023-09-04 20:54:22 -0300

    Update scriptstyle.js

--- docs/scriptstyle.js
+++ docs/scriptstyle.js
@@ -2,3 +2,9 @@ window.addEventListener('load', function () {
             var loadingScreen = document.querySelector('.loading-screen');
             loadingScreen.style.display = 'none';
         });
+
+$('img').mousedown(function (e) {
+  if(e.button == 2) { // right click
+    return false; // do nothing!
+  }
+});
